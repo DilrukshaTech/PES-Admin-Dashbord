0892d231aadd46a7e00ff5ad3e76488b1fcccbb2
Dilruksha <infolavonetech@gmail.com>
2025-06-21 16:49:13 +0530

    add controllers & routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-const express = require('express');
+import express from 'express';
+import { CategoryRoutes } from './routes/CategoryRoute';
+
 const app = express();
 
 app.use(express.json());
+app.use('/categories', CategoryRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
