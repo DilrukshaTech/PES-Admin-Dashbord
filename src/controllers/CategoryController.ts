748da2d47e6d61bc88fcd185f9c14925521396b2
Dilruksha <infolavonetech@gmail.com>
2025-06-21 21:47:43 +0530

    update category

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -1,15 +1,32 @@
 import { Request, Response } from "express";
 
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+
 
 const prisma = new PrismaClient();
 
-export const CreateCategory = async (req: Request, res: Response): Promise<void> => {
+export const CreateCategory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     if (!req.body.name) {
       res.status(422).json({
         error: "Category name is required",
       });
+    }
+
+    if (
+      await prisma.category.findUnique({
+        where: {
+          name: req.body.name,
+        },
+      })
+    ) {
+      res.status(422).json({
+        error: "Category already exists",
+      });
+      return;
     }
 
     const newCategory = await prisma.category.create({
@@ -18,10 +35,70 @@ export const CreateCategory = async (req: Request, res: Response): Promise<void>
       },
     });
     res.status(201).json(newCategory);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message || "Internal server error" });
     throw error;
   }
 };
 
+export const GetCategories = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const getAllCategories = await prisma.category.findMany();
+    res.status(200).json(getAllCategories);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message || "Internal server error" });
+    throw error;
+  }
+};
 
+export const UpdateCategory = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    if (
+      !(await prisma.category.findUnique({
+        where: {
+          id: parseInt(req.params.id),
+        },
+      }))
+    ) {
+      res.status(404).json({ error: "Category not found" });
+    }
+    if (!req.body.name) {
+      res.status(422).json({
+        error: "Category name is required",
+      });
+    }
+
+    if (
+      await prisma.category.findUnique({
+        where: {
+          name: req.body.name,
+        },
+      })
+    ) {
+      res.status(422).json({
+        error: "Category already exists",
+      });
+      return;
+    }
+
+    const updatedCategory = await prisma.category.update({
+      data: {
+        name: req.body.name,
+      },
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+
+    res.status(200).json({ updatedCategory });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message || "Internal server error" });
+    throw error;
+  }
+};
--- src/routes/CategoryRoute.ts
+++ src/routes/CategoryRoute.ts
@@ -1,8 +1,11 @@
 
 import express from 'express';
-import { CreateCategory } from '../controllers/CategoryController';
+import { CreateCategory,GetCategories,UpdateCategory } from '../controllers/CategoryController';
 
 const router = express.Router();
 router.post('/', CreateCategory);
+router.get('/', GetCategories);
+router.put('/:id',UpdateCategory)
+
 
 export const CategoryRoutes = router;
