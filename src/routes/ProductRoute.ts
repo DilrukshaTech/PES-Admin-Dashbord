ba5f3b69d7d40b01d79e45b63de6024bf0514ede
Dilruksha <infolavonetech@gmail.com>
2025-06-22 16:32:15 +0530

    product crud

--- src/routes/ProductRoute.ts
+++ src/routes/ProductRoute.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-const { CreateProduct,GetProducts  } = require('../controllers/ProductController');
+const { CreateProduct,GetProducts,GetProductById,UpdateProduct,DeleteProduct  } = require('../controllers/ProductController');
 
 const router = express.Router();
 router.post('/', CreateProduct);
 router.get('/', GetProducts);
+router.get('/:id', GetProductById);
+router.put('/:id', UpdateProduct); 
+router.delete('/:id',DeleteProduct)
 
 export const ProductRoutes = router;
